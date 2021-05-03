a9e5f8447e369fa74efcebba2ccbf87c9586e29c
Zimehr <f003xm6@plank.thayer.dartmouth.edu>
2021-05-02 21:09:48 -0400

    Final Changes

---
+++ crawler/README.md
@@ -0,0 +1,10 @@
+# Zimehr Abbasi
+## CS50 Spring 2021, Lab 4
+
+GitHub username: ZimehrAbbasi
+
+## ASSUMPTIONS
+The primary assumption this crawler works on is that the directory used to store the pages has already been created.
+
+## LIMITATIONS
+This program is limited by the amount of storage space available in the device.
--- crawler/testing.sh
+++ crawler/testing.sh
@@ -1,3 +1,16 @@
+// TEST CASES
+// MORE THAN 3 ARGUMENTS
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/letters 12 3
+// DEPTH OUT OF RANGE
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/letters 12
+// EXTERNAL URL
+./crawler http://www.google.com/ ../data/letters 10
+// INVALID DIRECTORY
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../dataa/ 1
+
+// VALGRIND TEST NO TOSCRAPE DEPTH 1
+valgrind ./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/index.html ../data/toscrape 1
+
 // LETTERS AT DEPTH 0, 1, 2, 10
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/letters 0
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html ../data/letters 1
